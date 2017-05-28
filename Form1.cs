5652611ee3f2b47933159151b32f1e33c4bdc7a5
geroCE17 <geroce@outlook.com>
2017-05-27 20:01:20 -0500

    Programa

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LC___Atencion_a_procesos
+{
+    public partial class Form1 : Form
+    {
+        static Random probabilidad = new Random();
+        Procesador procesador = new Procesador();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdLaunch_Click(object sender, EventArgs e)
+        {
+            int R, cont = 1, empty = 0;
+
+            while (cont <= 200)
+            {
+                R = probabilidad.Next(1, 5);
+                if (procesador.inicio != null)
+                    procesador.eliminarProceso();
+                if (R == 4)
+                    procesador.crearProceso();
+                if(procesador.inicio==null)
+                    empty++;
+                cont++;
+            }
+            procesador.pendientesSuma();
+            txtMostrar.Text = "Ciclos vacios: " + empty + "\r\n" + "Procesos pendientes: " + procesador.Pen + "\r\n" + "Ciclos restantes: " + procesador.suma;
+            empty = 0;
+            procesador.Pen = 0;
+            procesador.suma = 0;
+            procesador = new Procesador();
+        }
+    }
+}
---
+++ Procesador.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC___Atencion_a_procesos
+{
+    class Procesador
+    {
+        private int NoProceso = 0;
+        public Proceso inicio { get; set; }
+        public int Pen, suma;
+        private Proceso final;
+        private Proceso actual;
+        private static Random ciclos = new Random();
+
+        public void crearProceso()
+        {
+            Proceso nuevo = new Proceso();
+            nuevo.nombre = Convert.ToString("Proceso " + NoProceso);
+            nuevo.ciclos = ciclos.Next(4, 15);
+            if (inicio == null)
+            {
+                inicio = nuevo;
+                final = nuevo;
+            }
+            else
+            {
+                actual = inicio;
+                while (actual.siguiente != null)
+                {
+                    actual = actual.siguiente;
+                }
+                actual.siguiente = nuevo;
+                nuevo.anterior = actual;
+                final = nuevo;
+            }
+            NoProceso++;
+        }
+        public void eliminarProceso()
+        {
+            if (inicio == final && inicio.ciclos == 0)
+            {
+                inicio = null;
+                final = null;
+            }
+            else
+            {
+                if (inicio.ciclos == 0)
+                {
+                    inicio.siguiente.anterior = null;
+                    inicio = inicio.siguiente;
+                }
+                else
+                {
+                    inicio.ciclos--;
+                }
+            }
+        }
+
+        public void pendientesSuma()
+        {
+            if (inicio == null)
+            {
+                Pen = 0;
+                suma = 0;
+            }
+            else
+            {
+                actual = inicio;
+                while (actual != null)
+                {
+                    Pen++;
+                    suma = suma + actual.ciclos;
+                    actual = actual.siguiente;
+                }
+            }
+        }
+    }
+}
